060320345ffceac3c2a385b3d3427f6e571f8a76
troyboateng <dominic.asiedu1@stu.ucc.gh>
2022-08-07 12:49:01 +0100

    Practice codes

--- linked_list.py
+++ linked_list.py
@@ -8,5 +8,31 @@ class Node:
 
     def __init__(self, data):
         self.data = data
+
     def __repr__(self):
-        return "Node data: %s>" % self.data
+        return "Node data: %s>" % self.data     # "%s" is Python way of substituting something into a string.
+
+    class LinkedList:
+        """
+        Singly Linked list
+        """
+        head = None
+
+        def __init__(self):
+            self.head = None
+
+        def is_empty(self):
+            return self.head == None
+
+        def size(self):
+            """
+            Returns the number of nodes in the list
+            Takes O(n) time
+            """
+            current = self.head
+            count = 0
+
+            while current:
+                count += 1
+                current = current.next_node
+            return count
