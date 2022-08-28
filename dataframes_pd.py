ec25e4e281cd6afa0fdd2933c97e0655bfc2ee28
troyboateng <dominic.asiedu1@stu.ucc.gh>
2022-08-28 17:15:15 +0100

    Practice codes

--- dataframes_pd.py
+++ dataframes_pd.py
@@ -4,13 +4,19 @@ DataFrames allows one to store and manipulate tabular data in rows of
 observations and columns of variables
 """
 
-import pandas as pd
 
-dict1 = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
        "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
        "area": [8.516, 17.10, 3.286, 9.597, 1.221],
        "population": [200.4, 143.5, 1252, 1357, 52.98]}
 
-brics = pd.DataFrame(dict1)
+import pandas as pd
+brics = pd.DataFrame(dict)
 print(brics)
 
+# Set the index for brics
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+
+# Print out brics with new index values
+print(brics)
